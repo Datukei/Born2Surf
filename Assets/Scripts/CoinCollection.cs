58106f8f3adaf83f70662f1515aa263c9a2f5414
Daniel <kidanedaniel208@gmail.com>
2025-03-27 10:06:21 -0700

    BasicFunctions
    Added functional placeholder buttons: PLAY, SETTINGS, and QUIT

---
+++ Assets/Scripts/CoinCollection.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CoinCollection : MonoBehaviour
+{
+
+    private int Coin = 0;
+
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "Coin")
+        {
+            Coin++;
+            Debug.Log(Coin);
+            Destroy(other.gameObject);
+        }
+    }
+}
