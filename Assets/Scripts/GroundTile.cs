4c4cf36772d086916b01409f9d62c1b5b27f5230
ShanSewy <shanang.2717364@nv.ccsd.net>
2025-03-31 09:16:32 -0700

    Uploaded Sand Lvevel

---
+++ Assets/Scripts/GroundTile.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public class GroundTile : MonoBehaviour
+{
+    GroundSpawner groundSpawner;
+
+    void Start()
+    {
+        groundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
+        SpawnCoins();
+     
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        groundSpawner.SpawnTile();
+        Destroy(gameObject, 2);
+    }
+
+
+
+
+    void Update()
+    {
+        
+    }
+
+    public GameObject coinPrefab;
+
+
+    void SpawnCoins()
+    {
+
+        int coinsToSpawn = 10;
+        for (int i = 0; i < coinsToSpawn; i++) ;
+        GameObject temp = Instantiate(coinPrefab);
+        temp.transform.position = GetRandomPointInCollider(GetComponent<Collider>());
+
+
+
+    }
+
+    Vector3 GetRandomPointInCollider(Collider collider)
+    {
+
+        Vector3 point = new Vector3(
+            Random.Range(collider.bounds.min.x, collider.bounds.max.x),
+            Random.Range(collider.bounds.min.y, collider.bounds.max.y),
+            Random.Range(collider.bounds.min.z, collider.bounds.max.z)
+            );
+
+        if (point != collider.ClosestPoint(point))
+        {
+            point = GetRandomPointInCollider(collider);
+        }
+
+        point.y = 1;
+        return point;
+
+
+    }
+
+}
+    
+
+
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 5;
+    public Rigidbody rb;
+
+    float horizontalInput;
+
+    private void FixedUpdate()
+    {
+        Vector3 forwardMove = transform.forward * speed* Time.deltaTime;
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + forwardMove + horizontalMove);
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+}
