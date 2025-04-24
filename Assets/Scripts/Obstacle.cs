d181c45319a4473eb02265e9e50b79a781633c2b
jushwush <alcantarjoshua0@gmail.com>
2025-04-24 09:18:32 -0700

    ?

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -3,11 +3,16 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+<<<<<<< HEAD
     Player Player; 
 
 
 
+<<<<<<< HEAD
+    private void Start()
+=======
    private void Start()
+>>>>>>> parent of 2385c9c (test)
     {
         Player = GameObject.FindAnyObjectByType<Player>();
     }
