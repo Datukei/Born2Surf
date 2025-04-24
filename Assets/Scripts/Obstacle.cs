423c2b31d8b9683f473812f3e117fe488e6e85a3
jushwush <alcantarjoshua0@gmail.com>
2025-04-24 09:21:17 -0700

    fix 1

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -3,16 +3,10 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-<<<<<<< HEAD
+
     Player Player; 
 
-
-
-<<<<<<< HEAD
-    private void Start()
-=======
    private void Start()
->>>>>>> parent of 2385c9c (test)
     {
         Player = GameObject.FindAnyObjectByType<Player>();
     }
